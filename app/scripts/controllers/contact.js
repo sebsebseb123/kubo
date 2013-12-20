c0c56bd099a785988686d70ebe5b91ffe0d60bda
sebsebseb123 <sebastian.lesch@gmail.com>
2013-12-19 19:04:20 -0500

    Added other controllers and views.

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('designbykubocomApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/portfolio.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('designbykubocomApp')
+  .controller('PortfolioCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
